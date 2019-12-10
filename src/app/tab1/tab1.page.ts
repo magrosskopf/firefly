bc0ba27e8bf960c278704a557d2f2e2f8d5f8f12
magrosskopf <magrosskopf@web.de>
2019-12-10 11:26:24 +0100

    remove Camera

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -8,26 +7,8 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 })
 export class Tab1Page {
 
- options: CameraOptions = {
-    quality: 100,
-    destinationType: this.camera.DestinationType.FILE_URI,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE
-  } 
-  constructor(private camera: Camera) {
-    
-   }
+  constructor() {
 
-  makePhoto() {
-    this.camera.getPicture(this.options).then((imageData) => {
-      // imageData is either a base64 encoded string or a file URI
-      // If it's base64 (DATA_URL):
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
-      console.log(base64Image);
-      
-     }, (err) => {
-      // Handle error
-     });
   }
   
 

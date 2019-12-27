37ab039a22e937672d2dfa00fdb7f08d42a91645
ewice <henning.loewe@hotmail.de>
2019-12-27 17:14:40 +0100

    add deal interface

---
+++ src/app/_interfaces/deal.ts
@@ -0,0 +1,10 @@
+export interface Deal {
+    id: string;
+    title: string;
+    discription: string;
+    active: boolean;
+    picture: string;
+    location: string;
+    priceBefore: number;
+    priceNow: number;
+}

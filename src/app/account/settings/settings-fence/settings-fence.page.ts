7674ecc02acdaf689383ad7e850d419c5ca9520c
seeemwhykey <m.schmidt@naisone.de>
2020-01-20 13:18:34 +0100

    style-account feat(settingsFence): add save button functionality

--- src/app/account/settings/settings-fence/settings-fence.page.ts
+++ src/app/account/settings/settings-fence/settings-fence.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-settings-fence',
@@ -6,12 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings-fence.page.scss'],
 })
 export class SettingsFencePage implements OnInit {
-  sliderrange: number;
+  sliderrange = 0;
+  latitude = 0;
+  longitude = 0;
 
-  constructor() {
+  constructor(
+      public afAuth: AngularFireAuth,
+      public afDB: AngularFirestore
+  ) {
+    // TODO MS 20.01.20: insert Service getGeolocation() when branches are merged
   }
 
   ngOnInit() {
+  }
+
+  updateHomefence() {
+    const userId = this.afAuth.auth.currentUser.uid;
+    this.afDB.collection('customer').doc(userId).update({
+      homefence: [
+        this.latitude,
+        this.longitude,
+        this.sliderrange
+      ]
+    }).then(() => {
+      console.log('update homefence');
+    }).catch(error => {
+      console.log('update homefence didnt work', error);
+    });
   }
 
 }

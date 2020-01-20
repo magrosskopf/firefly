8315e07a878e2df04915b66dee4069da5216b009
seeemwhykey <m.schmidt@naisone.de>
2020-01-20 15:03:41 +0100

    style-store ui(shop-detail): WIP set up shop-detail

--- src/app/shop-detail/shop-detail.page.ts
+++ src/app/shop-detail/shop-detail.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRouteSnapshot, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { UserInfoService } from '../_services/user-info.service';
+import { StoreService } from '../_services/store.service';
 
 @Component({
   selector: 'app-shop-detail',
@@ -11,19 +12,30 @@ export class ShopDetailPage implements OnInit {
 
   seller;
 
-  constructor(private router: Router, private userinfo: UserInfoService) { }
+  store: Store = {
+    title: '',
+    description: '',
+    active: true,
+    location: ''
+  };
+
+  constructor(private router: Router,
+              private userinfo: UserInfoService,
+              private storeService: StoreService) {
+  }
 
   ngOnInit() {
     const pathArray = this.router.url.split('/');
     const pathId = pathArray[pathArray.length - 1];
     console.log(pathId);
-    this.userinfo.getSellerDataFromFirestore(pathId).subscribe(data => {
+    this.userinfo.getSellerDataFromFirestore(pathId)
+    .subscribe(data => {
       this.seller = data;
       console.log(data);
-      
-    })
-    
+    });
+    this.storeService.getStoreData(pathId)
+    .then((data) => {
+      this.store = data;
+    });
   }
-
-
 }

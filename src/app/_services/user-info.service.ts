042431219a344be7622c72856b0cde49cd828946
magrosskopf <magrosskopf@web.de>
2020-02-01 01:05:03 +0100

    add current user to updateNameAndPhoto

--- src/app/_services/user-info.service.ts
+++ src/app/_services/user-info.service.ts
@@ -25,6 +25,8 @@ export class UserInfoService {
   }
 
   updateNameAndPhoto(name, url) {
+      this.user = this.authentication.afAuth.auth.currentUser;
+
     if (this.user) {
       this.user.updateProfile({
         displayName: name,
